9e834d2ddc21a397e86c790b85c4511dda134206
qwqbot <ding00000804@gmail.com>
2021-11-26 12:59:58 +0800

    @laptop_arch_linux add html,lua,js lsp and treesitter

---
+++ lua/lsp/config/html.lua
@@ -0,0 +1,10 @@
+require'lspconfig'.html.setup{}
+
+--Enable (broadcasting) snippet capability for completion
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+require'lspconfig'.html.setup {
+  capabilities = capabilities,
+}
+
