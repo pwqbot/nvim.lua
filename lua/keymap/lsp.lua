d749207e913cead90e089dc334d22d9929992b1c
pwqbot <ding00000804@gmail.com>
2022-06-06 10:36:25 +0800

    update lsp

--- lua/keymap/lsp.lua
+++ lua/keymap/lsp.lua
@@ -27,7 +27,7 @@ local function keymappings(client, bufnr)
     vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
     vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
     vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ff', '<cmd>lua vim.lsp.buf.format()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ff', '<cmd>lua vim.lsp.buf.formatting_sync()<CR>', opts)
     vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ic', '<cmd>lua vim.lsp.buf.incoming_calls()<CR>', opts)
     vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>oc', '<cmd>lua vim.lsp.buf.outgoing_calls()<CR>', opts)
 end
