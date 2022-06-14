b07aea65d35cbc2349fa61328eae71081023ddc6
pwqbot <ding00000804@gmail.com>
2022-06-14 21:35:45 +0800

    use lazygit

--- lua/keymap/plugin.lua
+++ lua/keymap/plugin.lua
@@ -29,3 +29,16 @@ map('x', '<C-_>',
 
 --- quick motion ---
 map('n', 'f', function() require 'hop'.hint_words({}) end)
+
+local Terminal = require('toggleterm.terminal').Terminal
+local lazygit = Terminal:new({
+    cmd = "lazygit",
+    dir = "git_dir",
+    direction = "float",
+})
+
+function _lazygit_toggle()
+    lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<C-g>", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
