0fa9e51d039ed4fdf36c9af8257dd2e6bb60e532
pwqbot <ding00000804@gmail.com>
2022-09-29 22:02:19 +0800

    plugin: remove windows.nvim

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -65,18 +65,6 @@ require('packer').startup(
         use 'haya14busa/is.vim'
 
         -- auto adjust window size
-        use { "anuvyklack/windows.nvim",
-            requires = {
-                "anuvyklack/middleclass",
-                "anuvyklack/animation.nvim"
-            },
-            config = function()
-                vim.o.winwidth = 10
-                vim.o.winminwidth = 10
-                vim.o.equalalways = false
-                require('windows').setup()
-            end
-        }
 
         -- highlight cursorline
         use({
