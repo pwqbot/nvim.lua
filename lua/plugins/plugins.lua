40cf780c757384443ff0cc29e8a0b57f07376091
pwqbot <ding00000804@gmail.com>
2022-11-11 15:38:47 +0800

    packer: use default path

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,20 +1,37 @@
-local user_path = vim.fn.expand('~')
-Nvim_path = user_path .. [[/.config/nvim/]]
-local packer_start_path = Nvim_path .. 'pack/packer/start'
-local packer_path = packer_start_path .. '/packer.nvim'
-local packer_bootstrap
-if vim.fn.empty(vim.fn.glob(packer_path)) > 0 then
-    print("install packer")
-    packer_bootstrap = vim.fn.system({
-        'git', 'clone', '--depth', '1',
-        'https://github.com/wbthomason/packer.nvim', packer_path
-    })
+local local_packer = function()
+    local user_path = vim.fn.expand('~')
+
+    Nvim_path = user_path .. [[/.config/nvim/]]
+    local packer_start_path = Nvim_path .. 'pack/packer/start'
+    local packer_path = packer_start_path .. '/packer.nvim'
+    local packer_bootstrap
+    if vim.fn.empty(vim.fn.glob(packer_path)) > 0 then
+        print("install packer")
+        packer_bootstrap = vim.fn.system({
+            'git', 'clone', '--depth', '1',
+            'https://github.com/wbthomason/packer.nvim', packer_path
+        })
+    end
+    vim.opt.runtimepath:append(packer_start_path .. '/*')
 end
 
-vim.opt.runtimepath:append(packer_start_path .. '/*')
+
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 
 require('packer').init({
-    package_root = Nvim_path .. 'pack/',
+    -- package_root = Nvim_path .. 'pack/',
     profile = {
         enable = true,
         threshold = 0,
