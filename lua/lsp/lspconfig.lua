01a252de46aeaafab712fa46211e8189ddbf7854
pwqbot <ding00000804@gmail.com>
2022-11-15 17:29:54 +0800

    lsp: refactor clangd

--- lua/lsp/lspconfig.lua
+++ lua/lsp/lspconfig.lua
@@ -75,6 +75,7 @@ end
 local capabilities = require('cmp_nvim_lsp').default_capabilities(
     vim.lsp.protocol.make_client_capabilities()
 )
+capabilities.offsetEncoding = "utf-8"
 capabilities.textDocument.foldingRange = {
     dynamicRegistration = false,
     lineFoldingOnly = true
--- lua/lsp/servers.lua
+++ lua/lsp/servers.lua
@@ -41,16 +41,85 @@ local function get_pyls_plugins()
     return pylsp_plugins
 end
 
+local clangd = require("clangd_extensions").prepare {
+    server = {
+        cmd = {
+            "clangd",
+            "--background-index",
+            "--suggest-missing-includes",
+            "-compile-commands-dir=build",
+            "--clang-tidy",
+        },
+    },
+
+    extensions = {
+        -- defaults:
+        -- Automatically set inlay hints (type hints)
+        autoSetHints = true,
+        -- These apply to the default ClangdSetInlayHints command
+        inlay_hints = {
+            -- Only show inlay hints for the current line
+            only_current_line = false,
+            -- Event which triggers a refersh of the inlay hints.
+            -- You can make this "CursorMoved" or "CursorMoved,CursorMovedI" but
+            -- not that this may cause  higher CPU usage.
+            -- This option is only respected when only_current_line and
+            -- autoSetHints both are true.
+            only_current_line_autocmd = "CursorHold",
+            -- whether to show parameter hints with the inlay hints or not
+            show_parameter_hints = true,
+            -- prefix for parameter hints
+            parameter_hints_prefix = "<- ",
+            -- prefix for all the other hints (type, chaining)
+            other_hints_prefix = "=> ",
+            -- whether to align to the length of the longest line in the file
+            max_len_align = false,
+            -- padding from the left if max_len_align is true
+            max_len_align_padding = 1,
+            -- whether to align to the extreme right or not
+            right_align = false,
+            -- padding from the right if right_align is true
+            right_align_padding = 7,
+            -- The color of the hints
+            highlight = "Comment",
+            -- The highlight group priority for extmark
+            priority = 100,
+        },
+        ast = {
+            role_icons = {
+                type = "",
+                declaration = "",
+                expression = "",
+                specifier = "",
+                statement = "",
+                ["template argument"] = "",
+            },
+
+            kind_icons = {
+                Compound = "",
+                Recovery = "",
+                TranslationUnit = "",
+                PackExpansion = "",
+                TemplateTypeParm = "",
+                TemplateTemplateParm = "",
+                TemplateParamObject = "",
+            },
+
+            highlights = {
+                detail = "Comment",
+            },
+        },
+        memory_usage = {
+            border = "none",
+        },
+        symbol_info = {
+            border = "none",
+        },
+    },
+}
+
 M.servers = {
-    -- clangd = {
-    --     cmd = {
-    --         "clangd",
-    --         "--background-index",
-    --         "--suggest-missing-includes",
-    --         "-compile-commands-dir=build",
-    --         "--clang-tidy",
-    --     }
-    -- },
+    clangd = clangd,
     pyright = {
         python = {
             venvPath = find_python_venv()
