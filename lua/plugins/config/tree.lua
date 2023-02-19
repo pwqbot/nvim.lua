77f5a043c43495a13b799515af285840b34308de
qwqbot <ding00000804@gmail.com>
2023-02-19 06:59:17 +0000

    chore: require markdown in treesitter

--- lua/plugins/config/tree.lua
+++ lua/plugins/config/tree.lua
@@ -15,6 +15,8 @@ require 'nvim-treesitter.configs'.setup {
         "norg",
         "haskell",
         "regex",
+        "markdown",
+        "markdown_inline"
     },
     sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
     ignore_install = {}, -- List of parsers to ignore installing
