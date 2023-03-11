eb1f7063926f45404ae6af070a94b8796da6abb1
pwqbot <ding00000804@gmail.com>
2023-03-11 22:32:56 +0800

    plugin: add octo.nvim

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -140,5 +140,16 @@ return {
             "nvim-telescope/telescope.nvim",
             "MunifTanjim/nui.nvim",
         },
+    },
+    {
+        'pwntester/octo.nvim',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'nvim-telescope/telescope.nvim',
+            'kyazdani42/nvim-web-devicons',
+        },
+        config = function()
+            require "octo".setup()
+        end
     }
 }
