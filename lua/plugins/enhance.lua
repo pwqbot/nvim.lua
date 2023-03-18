a3309d48ecdc0795f8c0b21bbe1def5f7054a75e
pwqbot <ding00000804@gmail.com>
2023-03-18 15:16:38 +0800

    add indent-blankline.nvim

--- lua/plugins/enhance.lua
+++ lua/plugins/enhance.lua
@@ -29,6 +29,17 @@ return {
     -- automatically turn off search highlight
     'haya14busa/is.vim',
 
+    {
+        'lukas-reineke/indent-blankline.nvim',
+        config = function()
+            require("indent_blankline").setup {
+                -- for example, context is off by default, use this to turn it on
+                show_current_context = true,
+                show_current_context_start = true,
+            }
+        end
+    },
+
     -- resize window
     {
         'anuvyklack/windows.nvim',
