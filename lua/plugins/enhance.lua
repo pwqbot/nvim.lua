456c46b827b58f0ff88f2bf18483490ba5f88f9c
pwqbot <ding00000804@gmail.com>
2023-04-14 20:47:48 +0800

    fix dial.nvim

--- lua/plugins/enhance.lua
+++ lua/plugins/enhance.lua
@@ -24,7 +24,37 @@ return {
     },
 
     -- powerful c-a/c-x
-    'monaqa/dial.nvim',
+    {
+        'monaqa/dial.nvim',
+        keys = {
+            { "<C-a>", function() return require("dial.map").inc_normal() end, expr = true },
+            { "<C-x>", function() return require("dial.map").dec_normal() end, expr = true },
+            { "<C-a>", function() return require("dial.map").inc_visual() end, expr = true, mode = "v" },
+            { "<C-x>", function() return require("dial.map").dec_visual() end, expr = true, mode = "v" },
+        },
+        config = function()
+            local augend = require("dial.augend")
+            require("dial.config").augends:register_group {
+                -- default augends used when no group name is specified
+                default = {
+                    augend.integer.alias.decimal,  -- nonnegative decimal number (0, 1, 2, 3, ...)
+                    augend.integer.alias.hex,      -- nonnegative hex number  (0x01, 0x1a1f, etc.)
+                    augend.date.alias["%Y/%m/%d"], -- date (2022/02/19, etc.)
+                    augend.constant.alias.bool,    -- boolean value (true <-> false)
+                    augend.constant.new {
+                        elements = { "and", "or" },
+                        word = true,   -- if false, "sand" is incremented into "sor", "doctor" into "doctand", etc.
+                        cyclic = true, -- "or" is incremented into "and".
+                    },
+                    augend.constant.new {
+                        elements = { "&&", "||" },
+                        word = false,
+                        cyclic = true,
+                    },
+                },
+            }
+        end
+    },
 
     -- automatically turn off search highlight
     'haya14busa/is.vim',
