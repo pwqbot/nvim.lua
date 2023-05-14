e293af980794ac21cdf23a106ed310502e7b3c75
qwqbot <ding00000804@gmail.com>
2023-05-14 11:42:20 +0000

    copilot-cmp update

--- lua/plugins/complete.lua
+++ lua/plugins/complete.lua
@@ -58,16 +58,6 @@ return {
     {
         "zbirenbaum/copilot-cmp",
         lazy = true,
-        config = function()
-            require("copilot_cmp").setup({
-                method = "getCompletionsCycling",
-                formatters = {
-                    label = require("copilot_cmp.format").format_label_text,
-                    insert_text = require("copilot_cmp.format").format_insert_text,
-                    preview = require("copilot_cmp.format").deindent,
-                },
-            }
-            )
-        end
-    },
+        config = true,    
+    }
 }
