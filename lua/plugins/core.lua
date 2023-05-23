c8290411603a657276d8b7cfc2faabc2d8e6b4bb
qwqbot <ding00000804@gmail.com>
2023-05-23 03:22:14 +0000

    add nvim-osc52 for clipboard

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -159,6 +159,24 @@ return {
         },
     },
     {
+        'ojroques/nvim-osc52',
+        config = function()
+            local function copy(lines, _)
+                require('osc52').copy(table.concat(lines, '\n'))
+            end
+
+            local function paste()
+                return { vim.fn.split(vim.fn.getreg(''), '\n'), vim.fn.getregtype('') }
+            end
+
+            vim.g.clipboard = {
+                name = 'osc52',
+                copy = { ['+'] = copy, ['*'] = copy },
+                paste = { ['+'] = paste, ['*'] = paste },
+            }
+        end
+    },
+    {
         "nvim-neo-tree/neo-tree.nvim",
         cmd = "Neotree",
         dependencies = {
