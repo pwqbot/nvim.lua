2b6f010104889e402eff58df23932bb1b336a7f1
qwqbot <ding00000804@gmail.com>
2023-06-01 12:25:25 +0000

    remove async-task & async-run

--- lua/plugins/task.lua
+++ lua/plugins/task.lua
@@ -1,16 +1,4 @@
 return {
-    "skywind3000/asyncrun.vim",
-    {
-        "skywind3000/asynctasks.vim",
-        config = function()
-            vim.cmd [[ let g:asynctasks_term_reuse = 1 ]]
-            vim.cmd [[ let g:asynctasks_term_pos = 'floaterm_reuse' ]]
-            vim.cmd [[let g:asynctasks_term_rows = 6]]
-            vim.cmd [[let g:asynctasks_term_cols = 500]]
-            vim.cmd [[ let g:asynctasks_term_focus = 1 ]]
-        end
-    },
-    "GustavoKatel/telescope-asynctasks.nvim",
     {
         "Shatur/neovim-cmake",
         config = function()
