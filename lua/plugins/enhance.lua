053f2f961a54a8578eb0c92404eea7506119d7fa
qwqbot <ding00000804@gmail.com>
2023-06-02 06:58:13 +0000

    use toggleterm

--- lua/plugins/enhance.lua
+++ lua/plugins/enhance.lua
@@ -116,14 +116,58 @@ return {
     },
 
     {
-        "voldikss/vim-floaterm",
-        init = function()
-            vim.cmd [[let g:floaterm_keymap_toggle = '<C-t>']]
-            vim.cmd [[let g:floaterm_width = 0.8]]
-            vim.cmd [[let g:floaterm_height = 0.8]]
-            vim.cmd [[let g:floaterm_autoinsert = 1]]
+        'akinsho/toggleterm.nvim',
+        version = "*",
+        keys = {
+            { "<C-t>", "<cmd>ToggleTerm<CR>", noremap = true, silent = true },
+        },
+        opts = {
+            size = function(term)
+                if term.direction == "horizontal" then
+                    return 15
+                elseif term.direction == "vertical" then
+                    return vim.o.columns * 0.6
+                end
+            end,
+            insert_mappings = true,
+            terminal_mappings = true,
+            direction = 'vertical',
+            open_mapping = [[<c-t>]],
+            winbar = {
+                enabled = true,
+                name_formatter = function(term) --  term: Terminal
+                    return term.name
+                end
+            },
+            autochdir = true,
+            float_opts = {
+                -- The border key is *almost* the same as 'nvim_open_win'
+                -- see :h nvim_open_win for details on borders however
+                -- the 'curved' border is a custom border type
+                -- not natively supported but implemented in this plugin.
+                border = 'curved',
+                -- like `size`, width and height can be a number or function which is passed the current terminal
+                winblend = 3,
+            },
+            auto_scroll = false,
+        },
+        config = function(_, opts)
+            require("toggleterm").setup(opts)
+            function _G.set_terminal_keymaps()
+                local opts = { buffer = 0 }
+                vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+                vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+                vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+                vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+                vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+                vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+            end
+
+            -- if you only want these mappings for toggle term use term://*toggleterm#* instead
+            vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
         end
     },
+
     {
         'willothy/flatten.nvim',
         config = true,
